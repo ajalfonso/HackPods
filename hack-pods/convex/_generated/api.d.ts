073d87a2d913a127d0612596a59901bc44e2b78b
AJ Alfonso <24573242+ajalfonso@users.noreply.github.com>
2024-09-15 00:11:00 -0400

    added db queries for members and teams

--- hack-pods/convex/_generated/api.d.ts
+++ hack-pods/convex/_generated/api.d.ts
@@ -15,7 +15,9 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as members from "../members.js";
 import type * as tasks from "../tasks.js";
+import type * as teams from "../teams.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -26,7 +28,9 @@ import type * as tasks from "../tasks.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  members: typeof members;
   tasks: typeof tasks;
+  teams: typeof teams;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
